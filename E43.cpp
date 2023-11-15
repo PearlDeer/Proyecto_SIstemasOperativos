cea849849dbcba6341f02261c74c2a073ab12932
mromo066 <mromo066@gmail.com>
2023-11-15 15:57:45 -0600

    se agrega commit dos desde mromo

--- E43.cpp
+++ E43.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main(){
     cout<<endl<<"borrando todo lo anterior para subir los cambios al branch principal desde mromo";
 
+    cout<<endl<<"Edicion para el commit dos de la prueba";
     return 0;
 }
