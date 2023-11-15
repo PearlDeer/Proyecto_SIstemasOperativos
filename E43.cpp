e2c00775e19f111c5ab94daa3f9631175f20f02a
mromo066 <mromo066@gmail.com>
2023-11-15 16:13:24 -0600

    editando ahora el branch de mromo para luego cargarlo al branch principal

--- E43.cpp
+++ E43.cpp
@@ -7,5 +7,7 @@ int main(){
     cout<<endl<<"borrando todo lo anterior para subir los cambios al branch principal desde mromo";
 
     cout<<endl<<"Edicion para el commit dos de la prueba";
+
+    cout<<endl<<"Se esta editando para cargar luego del primer merge de mromo";
     return 0;
 }
